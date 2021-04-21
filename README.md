0cb5142d242233c08d7d884519dd7ced613199f6
Pavaman Dabeer <dpavaman@gmail.com>
2021-04-22 00:54:16 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Whack-a-mole.
+
+
+* Whack a mole is a Game that requires user to click on a fast moving Mole across the grid of 9 boxes.
+* Timer and score are being displayed on the screen to track the number of right clicks done in a given slot of time. 
+* Timer and movement of mole will stop once the timer reaches 0
+
+
+![Demo](Demo/demo.gif)
---
+++ script.js
@@ -0,0 +1,52 @@
+const squares = document.querySelectorAll('.square');
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
+
+
+let result = 0;
+let hitPosition;
+let currentTime = 60;
+let timerId = null;
+
+function randomSquare() {
+
+    squares.forEach((square) => {
+        square.classList.remove('mole')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    randomSquare.classList.add("mole")
+    hitPosition = randomSquare.id;
+
+}
+
+squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+        if (square.id == hitPosition) {
+            result++;
+            score.textContent = result;
+            hitPosition = null;
+        }
+    })
+})
+
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime;
+
+    if (currentTime === 0) {
+        clearInterval(timerId)
+        clearInterval(counterId)
+        alert("Game Over.. Your Score is " + result)
+    }
+}
+
+
+let counterId = setInterval(countDown, 1000)
